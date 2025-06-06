2dde50e3a65bca7777aa8dfacc146b7f2e532dab
paodelonga <72471292+paodelonga@users.noreply.github.com>
2025-06-06 19:45:09 -0300

    202506061944

---
+++ modelagem-situacao_de_aprendizagem/sw_acesso_ti.bash
@@ -0,0 +1,45 @@
+#!/usr/bin/bash
+
+# Habilita modo privilegiado
+echo -e "enable"
+
+# Entra no modo de configuração
+echo -e "configure terminal\n"
+
+# Definir hostname
+echo -e "hostname SW_AC_RH\n"
+
+# Criar VLAN para a rede
+echo -e "vlan 10"
+echo -e "name REDE_RH\n"
+
+# Atribuir VLANs as portas a serem utilizadas
+echo -e "interface range fa 0/1-6"
+echo -e "switchport access vlan 10"
+echo -e "duplex auto"
+echo -e "speed auto"
+echo -e "no shutdown\n"
+
+echo -e "interface fa 0/24"
+echo -e "switchport access vlan 10"
+echo -e "duplex auto"
+echo -e "speed auto"
+echo -e "no shutdown\n"
+
+# Desligar e reconfigurar as portas não utilizadas
+echo -e "interface range fa 0/7-23"
+echo -e "switchport access vlan 1"
+echo -e "duplex auto"
+echo -e "speed auto"
+echo -e "shutdown\n"
+
+echo -e "interface ra gi 0/1-2"
+echo -e "switchport access vlan 1"
+echo -e "duplex auto"
+echo -e "speed auto"
+echo -e "shutdown"
+echo -e "exit\n"
+
+# Salva as modificações feitas
+echo -e "exit"
+echo -e "copy running-config startup-config"
